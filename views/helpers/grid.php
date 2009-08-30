18de974daff9c145ff5689b12278052ca769d972
Craig Morris <craig.michael.morris@gmail.com>
2009-08-31 08:29:22 +1000

    Fixed bug trying to include an element from a plugin. Added sequence behavior

--- views/helpers/grid.php
+++ views/helpers/grid.php
@@ -2,21 +2,21 @@
 /**
 * GridHelper allows you to create HTML valid grids using tables from an array of data.
 * An element is called to actually display each element in the array.
-* 
-* The grid helper will automatically finish and start new rows based on the number of 
-* columns you want. On the last row it will also, finish the number of columns with 
+*
+* The grid helper will automatically finish and start new rows based on the number of
+* columns you want. On the last row it will also, finish the number of columns with
 * empty cells.
-* 
+*
 * Each row will be available in the element as $data. The element should contain the <td>
 * tag (this is so it can set class names and whatever else you wish)
-* 
+*
 * Example Usage (view):
 * echo $grid->grid($products, array('element' => 'grid_product', 'cols' => 3))
-* 
+*
 * Settings:
 *
 * element
-* The element called to display each row in your data. 
+* The element called to display each row in your data.
 * default: "grid" (this most likely wont be in your view!)
 *
 * cols
@@ -34,13 +34,18 @@ class GridHelper extends AppHelper
 	function grid($rows, $opts)
 	{
 		$view =& ClassRegistry:: getObject('view');
-		$default = array(
-			'element' => 'grid',
-			'cols' => 4
-		);
-		$opts = array_merge($default, $opts);
-		extract($opts);
-		
+
+		$cols = 4;
+		if ( isset($opts['cols']) ) {
+			$cols = $opts['cols'];
+			unset($opts['cols']);
+		}
+
+		$element = 'grid';
+		if ( isset($opts['element']) ) {
+			$element = $opts['element'];
+			unset($opts['element']);
+		}
 		?>
 		<tr>
 			<?php
@@ -49,20 +54,20 @@ class GridHelper extends AppHelper
 				$last = end($rows);
 				foreach ($rows as $row)
 				{
-					echo $view->element($element, array('data' => $row));
+					echo $view->element($element, am(array('data' => $row), $opts));
 					if ( $i % $cols === 0 && $row != $last ) {
 						echo '</tr><tr>';
 					}
 					$first = false;
 					$i++;
 				}
-				
+
 				// $i is always one more than the cells we have made
 				$i--;
-				
+
 				$soFar = $i % $cols;
 				$toGo = $cols - $soFar;
-				
+
 				if ( $soFar ) for ( $j = 0; $j < $toGo; $j++ ) {
 					echo '<td>&nbsp;</td>';
 				}
